877b338f660e2b3a0ee0559710605888d146554a
Yuming Long <305248291@qq.com>
2022-12-20 15:29:57 -0500

    chore: imort should be from typing library

--- unstructured/partition/pdf.py
+++ unstructured/partition/pdf.py
@@ -1,18 +1,6 @@
 import requests  # type: ignore
 
-import sys
-
-if sys.version_info < (3, 8):
-    from typing_extensions import (
-        List,
-        Optional,
-        Union,
-        Tuple,
-        Mapping,
-        BinaryIO,
-    )  # pragma: no cover
-else:
-    from typing import List, Optional, Union, Tuple, Mapping, BinaryIO  # pragma: no cover
+from typing import List, Optional, Union, Tuple, Mapping, BinaryIO
 
 from unstructured.documents.elements import Element
 
